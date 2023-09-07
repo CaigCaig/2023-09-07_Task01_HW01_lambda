526dfc840635c1934aa1b60ee6a6683f6e4d1acc
CaigCaig <caig@inbox.ru>
2023-09-07 22:26:48 +0300

    Initial commit

---
+++ 2023-09-07_Task01_HW01_lambda.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	auto arr = { 4, 7, 9, 14, 12 };
+
+	struct {
+		void operator()(int i) const { cout << i << ' '; }
+	} in_show;
+
+	cout << "Входные данные: ";
+	for_each(arr.begin(), arr.end(), in_show);
+	cout << endl;
+
+	struct {
+		void operator()(int i) const
+		{ 
+			switch (i % 2)
+			{
+			case 0:
+				cout << i << ' ';
+				break;
+			case 1:
+				cout << i * 3 << ' ';
+			}
+		}
+	} out_show;
+
+	cout << "Выходные данные: ";
+	for_each(arr.begin(), arr.end(), out_show);
+	cout << endl;
+
+	system("pause");
+}
